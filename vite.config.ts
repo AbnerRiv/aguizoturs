8dbb6e0780266633713b0fb1f2fe531981159008
Abner Rivera <abnertinoco@gmail.com>
2024-05-09 00:18:33 -0600

    fixing deploy error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [vue(), tailwindcss()],
+  define: { 'process.env': {} },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
+      alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.ts',
+          '.tsx',
+          '.vue',
+      ],
+  },
+  server: {
+      port: 3000,
+      open: true,
+      cors: true,
   }
 })
